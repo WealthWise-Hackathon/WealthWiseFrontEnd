fadf0a049240841635df78756388e0062a5c98f7
10_days_till_xmas <159474213+10-days-till-xmas@users.noreply.github.com>
2025-02-09 13:15:12 +0000

    Begin implementation of a HeaderSideBar Component, that'd include the header and sidebar in a single component

---
+++ wealthwisefrontend/src/Components/HeaderSideBar.tsx
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import SideNavBar from './SideNavBar.tsx';
+import './HeaderSideBar.tsx.css';
+interface HeaderSideBarProps {
+    children: React.ReactNode;
+}
+
+const HeaderSideBar: React.FC<HeaderSideBarProps> = ({ children }) => {
+    return (
+        <>
+            <header className="header">
+                <h1>WealthWise Financial Advisors</h1>
+            </header>
+            <SideNavBar />
+            <div>
+                {children}
+            </div>
+        </>
+        
+    );
+}
+
+HeaderSideBar.displayName = 'HeaderSideBar';
+
+export default React.memo(HeaderSideBar);
