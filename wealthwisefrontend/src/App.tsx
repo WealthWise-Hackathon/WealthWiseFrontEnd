e5496f6fcdf9bdbaa563a95bcd056c18a15f4a6f
10_days_till_xmas <159474213+10-days-till-xmas@users.noreply.github.com>
2025-02-09 13:13:47 +0000

    Implement simple version of SideNavBar, and clean up files

--- wealthwisefrontend/src/App.tsx
+++ wealthwisefrontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import SideNavBar from './Components/SideNavBar.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <header className="header">
+                <h1>WealthWise Financial Advisors</h1>
+            </header>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <div className="main-content">
+                <div className="sidebar">
+                    <SideNavBar />
+                </div>
+
+                <div className="content">
+                    <h1>Compound interest calculator</h1>
+                    <div className="mortgage-content">
+                        <p>See if we can help you with your personal finance</p>
+                        <button className="mortgage_button">Check Now</button>
+                        <div style={{ marginBottom: '5em' }}></div>
+                        <p>
+                            lots of text
+                            <div style={{ marginBottom: '10em' }}></div>
+                            more texts
+                            <div style={{ marginBottom: '10em' }}></div>
+                            more!!!!
+                            <div style={{ marginBottom: '50em' }}></div>
+                            even more!!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
